02dd920f5fec1cfd7cee63d6505c252e89536346
rafailovv <74158171+rafailovv@users.noreply.github.com>
2021-08-20 16:19:44 +0300

    Полностью добавили функционал

--- js/main.js
+++ js/main.js
@@ -1,13 +1,42 @@
 $(document).ready(function () {
-	var currentFloor = 2;
     var floorPath = $(".home-image path");
+	var currentFloor = 2;
     var counterUp = $(".counter-up");
     var counterDown = $(".counter-down");
+    var modal = $('.modal');
+    var modalCloseButton = $('.modal-close-button');
+    var viewFlatsButton = $('.view-flats');
+    var flatPath = $(".flats path");
+    var flatItem = $(".flat-item");
+    var flatLink = $(".flat-list li a");
+
 	floorPath.on("mouseover", function () {
         floorPath.removeClass("current-floor");
 		currentFloor = $(this).attr("data-floor");
 		$(".counter").text(currentFloor);
 	});
+
+    flatPath.on("mouseover", function () {
+        flatItem[$(this).attr("data-flat") - 1].children[0].classList.add("active");
+        $(this)[0].style.opacity = 1;
+    });
+
+    flatPath.on("mouseout", function() {
+        flatItem[$(this).attr("data-flat") - 1].children[0].classList.remove("active");
+        $(this)[0].style.opacity = 0;
+    });
+
+    flatLink.on("mouseover", function() {
+        flatPath[$(this)[0].getAttribute("data-flat") - 1].style.opacity = 1;
+    });
+
+    flatLink.on("mouseout", function() {
+        flatPath[$(this)[0].getAttribute("data-flat") - 1].style.opacity = 0;
+    });
+
+    floorPath.on("click", toggleModal);
+    modalCloseButton.on("click", toggleModal);
+    viewFlatsButton.on("click", toggleModal);
 
     counterUp.on("click", function () {
         if (currentFloor < 18) {
@@ -28,4 +57,8 @@ $(document).ready(function () {
             $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
         }
     });
+
+    function toggleModal() {
+        modal.toggleClass("is-open");
+    }
 });
